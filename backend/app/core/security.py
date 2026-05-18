7c7052441a4e0b5b56f243ff19e09f76860c36a9
Leonardo Sanchez <leonardoeliassancheznolazco0@gmail.com>
2026-05-17 21:13:51 -0500

    feat: autenticacion JWT con registro y login funcionando

---
+++ backend/app/core/security.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verificar_contrasena(contrasena_plana, contrasena_hash):
+    return pwd_context.verify(contrasena_plana, contrasena_hash)
+
+def hashear_contrasena(contrasena):
+    return pwd_context.hash(contrasena)
+
+def crear_token(data: dict):
+    datos = data.copy()
+    expira = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    datos.update({"exp": expira})
+    return jwt.encode(datos, SECRET_KEY, algorithm=ALGORITHM)
+
+def verificar_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
